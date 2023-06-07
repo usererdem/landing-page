da38078651583c9703ec901c6fd3b964565856f2
usererdem <erdemelvan35@gmail.com>
2023-06-07 13:01:46 +0300

    AboutSectionStart

---
+++ src/components/AboutSection.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <div>
+      <div className='description'>
+        <div className='title'>
+          <div className='hide'>
+            <h2>We work to make</h2>
+          </div>
+          <div className='hide'>
+            <h2>
+              you <span>dreams</span>
+            </h2>
+          </div>
+          <div className='hide'>
+            <h2>come true.</h2>
+          </div>
+        </div>
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact Us</button>
+      </div>
+      <div className="image">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
