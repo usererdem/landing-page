1d0d5d7517debdd2d415dd5cbbb06bfc920fbc74
erdemelvan <erdemelvan35@gmail.com>
2023-10-02 14:11:48 +0300

    styling-faqsection layout styling added

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import styled from "styled-components";
+import { Layout } from "../styles";
+
 const FaqSection = () => {
   return (
-    <div className='faq'>
+    <Faq className='faq'>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -16,33 +19,72 @@ const FaqSection = () => {
             minima.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
 
-      <div className="question">
+      <div className='question'>
         <h4>Daily Schedule</h4>
-        <div className="answer">
+        <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, minima.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            minima.
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
 
-      <div className="question">
+      <div className='question'>
         <h4>Different Payment Methods</h4>
-        <div className="answer">
+        <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, minima.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            minima.
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
 
-      <div className="question">
+      <div className='question'>
         <h4>What Product do you offer?</h4>
-        <div className="answer">
+        <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, minima.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+            minima.
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(Layout)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
