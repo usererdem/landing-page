5cb2eba4fc88ab392b50a3bca637b18fa71bea3f
erdemelvan <erdemelvan35@gmail.com>
2023-10-04 15:22:08 +0300

    feat: about section animation on page load

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,7 +1,9 @@
 import React from "react";
 import home1 from "../img/home1.png";
 import { Layout, Description, Image, Hide } from "../styles";
+// Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim, pageAnimation } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -9,27 +11,27 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
 
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span>
             </motion.h2>
           </Hide>
 
           <Hide>
-            <motion.h2>come true.</motion.h2>
+            <motion.h2 variants={titleAnim}>come true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt='guy with a camera' />
+        <motion.img variants={photoAnim} src={home1} alt='guy with a camera' />
       </Image>
     </Layout>
   );
