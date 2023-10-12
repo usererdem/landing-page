5794af3ae4ff7e96c342fb9a48db7bc595eb7aed
erdemelvan <erdemelvan35@gmail.com>
2023-10-12 04:35:40 +0300

    feat: faq section animation and toggle functionality added

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,7 +1,9 @@
 import React from "react";
-
 import styled from "styled-components";
+import { LayoutGroup } from "framer-motion";
+
 import { Layout } from "../styles";
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
   return (
@@ -10,53 +12,44 @@ const FaqSection = () => {
         Any Questions <span>FAQ</span>
       </h2>
 
-      <div className='question'>
-        <h4>How Do I Start?</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-            minima.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-
-      <div className='question'>
-        <h4>Daily Schedule</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-            minima.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-
-      <div className='question'>
-        <h4>Different Payment Methods</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-            minima.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-
-      <div className='question'>
-        <h4>What Product do you offer?</h4>
-        <div className='answer'>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-            minima.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <LayoutGroup>
+        <Toggle title='How Do I Start?'>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+              reprehenderit perferendis sunt magni dolores ratione.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title='What Products do you offer?'>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+              reprehenderit perferendis sunt magni dolores ratione.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title='Diferrent Payment Methods'>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+              reprehenderit perferendis sunt magni dolores ratione.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title='Daily Schedule'>
+          <div className='answer'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+              reprehenderit perferendis sunt magni dolores ratione.
+            </p>
+          </div>
+        </Toggle>
+      </LayoutGroup>
     </Faq>
   );
 };
---
+++ src/components/Toggle.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { fade } from "../animation";
+
+const Toggle = ({ children, title }) => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <StyledToggle layout onClick={() => setToggle(!toggle)}>
+      <motion.h4 layout>{title}</motion.h4>
+      {toggle ? <motion.div variants={fade}>{children}</motion.div> : ""}
+      <div className='faq-line'></div>
+    </StyledToggle>
+  );
+};
+
+const StyledToggle = styled(motion.div)`
+  cursor: pointer;
+  h4 {
+    padding: 2rem 0rem;
+  }
+`;
+
+export default Toggle;
